797c5c6f029a0c1809757d59f6838cd8ab1f2496
TicDollyRow <frauaja@yandex.ru>
2025-10-06 20:27:04 +0300

    Task6

--- Lecture01/src/main/java/com/algo/Task6.java
+++ Lecture01/src/main/java/com/algo/Task6.java
@@ -48,52 +48,28 @@ public class Task6 {
 
             rowSum[i][0] = rowVal;
             rowSum[i][1] = i;
-//            if(!treeMap.isEmpty()) {
-//                int maxLineVal = treeMap.firstKey();
-//                if (maxLineVal - rowVal > 1) continue;
-//            }
-//            List<Integer> list = new ArrayList<>();
-//            if(treeMap.containsKey(rowVal)){
-//                list = treeMap.get(rowVal);
-//                list.add(i);
-//            }else{
-//                list.add(i);
-//                treeMap.put(rowVal, list);
-//            }
         }
 
         Arrays.sort(columnSum, (o1, o2) -> o1[0] - o2[0]);
         Arrays.sort(rowSum, (o1, o2) -> o1[0] - o2[0]);
-
         int max = Integer.MIN_VALUE;
-//        int maxLineVal = treeMap.firstKey();
         int maxLineVal = rowSum[0][0];
         int maxColumnVal = columnSum[0][0];
-//        for(Integer val : treeMap.keySet()){
-        for(int i = 0; i < n; i++){
-            int val = rowSum[i][0];
-            if(maxLineVal - val > 1) break;
-
-//            List<Integer> list = treeMap.get(val);
-//            int diff = 0;
-//            for(int col = 0; col < m; col++){
-//                if(maxColumnVal - columnSum[col][0] > 1) break;
-//                for(Integer row:list){
-//                    diff = val - columnSum[col][0];
-//                    if(questionSet.contains(row+","+columnSum[col][1])){
-//                        diff -= 2;
-//                    }
-//                    max = Math.max(max, diff);
-//                }
-//            }
-            int diff = 0;
-            for(int col = 0; col < m; col++){
-                if(maxColumnVal - columnSum[col][0] > 1) break;
-                diff = val - columnSum[col][0];
-                if(questionSet.contains(rowSum[i][1]+","+columnSum[col][1])){
+        if(questionSet.size() == n*m){
+            max = maxLineVal- maxColumnVal-2;
+        }else {
+            for (int i = 0; i < n; i++) {
+                int val = rowSum[i][0];
+                if (maxLineVal - val > 1) break;
+                for (int col = 0; col < m; col++) {
+                    int diff = 0;
+                    if (maxColumnVal - columnSum[col][0] > 1) break;
+                    diff = val - columnSum[col][0];
+                    if (questionSet.contains(rowSum[i][1] + "," + columnSum[col][1])) {
                         diff -= 2;
+                    }
+                    max = Math.max(max, diff);
                 }
-                max = Math.max(max, diff);
             }
         }
         System.out.println(max);
